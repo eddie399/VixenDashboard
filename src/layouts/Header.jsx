041a109a0ca7360c7d83b446bbfddde5dc64c3a7
eddie399 <eddiendamera1@gmail.com>
2025-04-01 19:33:34 +0100

    another trial

---
+++ src/layouts/Header.jsx
@@ -0,0 +1,61 @@
+import {useTheme} from "../hooks/use-theme"
+import ProfileImg from "../assets/profile-image.jpg";
+
+
+import { Bell, ChevronsLeft, Moon, Search, Sun } from "lucide-react";
+import PropTypes from "prop-types";
+
+
+export const Header = ({collapsed, setCollapsed}) => {
+
+    const{theme, setTheme} = useTheme();
+    return ( 
+        <header className="relative flex h-[60px] items-center justify-between bg-white px-4 shadow-md transition-colors dark:bg-slate-900">
+            <div className="flex items-center gap-x-3">
+                <button
+                    className="btn-ghost size-10"
+                    onClick={() => setCollapsed(!collapsed)}
+                >
+                    <ChevronsLeft className={collapsed && "rotate-180"} />
+                </button>
+                <div className="input">
+                    <Search 
+                        size={20}
+                        className="text-slate-300"
+                    />
+                    <input 
+                        type="text"
+                        name="search"
+                        id="search"
+                        placeholder="search..."
+                        className="w-full bg-transparent text-slate-900 outline-0 placeholder:text-slate-300 dark:to-slate-50"
+                    />
+                </div>
+            </div>
+            <div className="flex items-center gap-x-3">
+                <button className="btn-ghost size-10"
+                    onClick={() => 
+                        setTheme(theme === "light" ? "dark" : "light")
+                    }
+                >
+                    <Sun size={20} className="dark:hidden"/>
+                    <Moon size={20} className="hidden dark:block" />    
+                </button>
+                <button className="btn-ghost size-10">
+                    <Bell size={20}/>
+                </button>
+                <button className="size-10 overflow-hidden rounded-full">
+                    <img src={ProfileImg} alt="profle" />
+                </button>
+            </div>
+            
+        </header>
+    );
+
+
+}
+
+Header.PropTypes = {
+    collapsed: PropTypes.bool,
+    setCollapsed: PropTypes.func,
+}
---
+++ src/layouts/sidebar.jsx
@@ -0,0 +1,67 @@
+import { forwardRef } from "react";
+import {navbarLinks} from "../constants/index"
+import { cn } from "../utils/cn";
+import PropTypes from "prop-types";
+import LogoLight from "../assets/favicon-light.svg"
+import LogoDark from "../assets/favicon-dark.svg"
+import { NavLink } from "react-router-dom";
+
+
+export const Sidebar = forwardRef (({collapsed}, ref) => {
+    
+    return  (
+        <aside 
+            ref={ref} 
+            className={cn(
+                "fixed z-[100] flex h-full w-[240px] flex-col overflow-x-hidden border-r border-slate-300 bg-white [transition:_width_300ms_cubic-bezier(0.4,_0,_0.2,_1),_left_300ms_cubic-bezier(0.4,_0,_0.2,_1),_background-color_150ms_cubic-bezier(0.4,_0,_0.2,_1),_border_150ms_cubic-bezier(0.4,_0,_0.2,_1)] dark:border-slate-700 dark:bg-slate-900",
+                collapsed ? "md:w-[70px] md:items-center" : "md:w-[240px]",
+                collapsed ? "max-md:-left-full" : "max-md:left-0"
+            )}
+        >
+            <div className="flex gap-x-3 p-3">
+                <img 
+                 src={LogoLight}
+                 alt="VIXEN" 
+                 className="dark:hidden"
+                />
+
+                <img 
+                 src={LogoDark}
+                 alt="VIXENSHOPPERS" 
+                 className="dark:hidden"
+                />
+                {!collapsed && <p className="text-lg font-medium text-slate-900 transition-colors dark:text-slate-50">VIXENSHOPPERS</p>}
+            </div>
+            <div className="flex w-full flex-col gap-y-4 overflow-y-auto overflow-x-hidden p-3[scrorllbar-width:_thin]">
+                {navbarLinks.map((navbarLinks) => (
+                    <nav
+                        key={navbarLinks.title}
+                        className={cn("sidebar_group")}
+                    >
+                        <p className={cn("sidebar-group-title", collapsed && "md:w-[45px]")}>{navbarLinks.title}</p>
+                        {navbarLinks.links.map((link) =>(
+                            <NavLink
+                                key={link.label}
+                                to={link.path}
+                                className={cn("sidebar-item", collapsed && "md:w-[45px]")}
+                            >
+                                <link.icon 
+                                    size={22}
+                                    className="flex-shrink-0"
+                                />
+                                {!collapsed && <p className="whitespace-nowrap">{link.label}</p>}
+                            </NavLink>
+                        ))}
+                    </nav>
+                ))}
+            </div>
+        </aside>
+    
+    )
+});
+
+Sidebar.displayName ="sidebar"
+
+Sidebar.propTypes = {
+    collapsed: PropTypes.bool,
+};
